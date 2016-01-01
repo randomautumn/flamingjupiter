545d59ab7ca4a94fab6406f1978cd4b231b2105b
Emory Merryman <emory.merryman@gmail.com>
2016-01-01 16:09:41 -0500

    added pass

--- provisioners/smalljazz.sh
+++ provisioners/smalljazz.sh
@@ -3,6 +3,6 @@
 export PATH=/vagrant/scripts:${PATH} &&
 install_ivoryomega.sh &&
 dnf_update.sh &&
-dnf_install.sh gpg git libxml2-devel libjpeg-devel python gcc-c++ make openssl-devel gcc ruby ruby-devel rubygems tree npm nodejs ncurses ncurses-devel wget glibc-static &&
+dnf_install.sh gpg git libxml2-devel libjpeg-devel python gcc-c++ make openssl-devel gcc ruby ruby-devel rubygems tree npm nodejs ncurses ncurses-devel wget glibc-static pass &&
 private_user.sh &&
 true
