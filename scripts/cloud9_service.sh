705e3df8861c1d4233d51cc67d05fec578cbc84d
Emory Merryman <emory.merryman@gmail.com>
2016-01-01 17:25:26 -0500

    added EOF heredoc tag

--- scripts/cloud9_service.sh
+++ scripts/cloud9_service.sh
@@ -6,7 +6,7 @@ mkdir workspace &&
 Description=Magenta Knife Cloud9 Service
 
 [Service]
-ExecStart=/usr/bin/su --login emory --command /usr/bin/node /home/emory/c9sdk/server.js --port 26647 --listen 0.0.0.0 --auth emory:emory -w /home/emory/workspace
+ExecStart=/usr/bin/su --login emory --command "/usr/bin/node /home/emory/c9sdk/server.js --port 26647 --listen 0.0.0.0 --auth emory:emory -w /home/emory/workspace"
 
 [Install]
 WantedBy=multi-user.target
