ab8db60d0ceaf371099e431e6ea7aee68d1dd095
Emory Merryman <emory.merryman@gmail.com>
2016-01-01 16:50:54 -0500

    added EOF heredoc tag

--- provisioners/smalljazz.sh
+++ provisioners/smalljazz.sh
@@ -4,6 +4,6 @@ export PATH=/vagrant/scripts:${PATH} &&
 install_ivoryomega.sh &&
 dnf_update.sh &&
 dnf_install.sh gpg git libxml2-devel libjpeg-devel python gcc-c++ make openssl-devel gcc ruby ruby-devel rubygems tree npm nodejs ncurses ncurses-devel wget glibc-static pass &&
-# private_user.sh &&
-# cloud9_service.sh &&
+private_user.sh &&
+cloud9_service.sh &&
 true
