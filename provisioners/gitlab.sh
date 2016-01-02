09ced33540deb31bcd5e372691944b4641a00439
Emory Merryman <emory.merryman@gmail.com>
2016-01-02 04:36:14 -0500

    using wget instead of curl

--- provisioners/gitlab.sh
+++ provisioners/gitlab.sh
@@ -4,7 +4,7 @@ dnf install --assumeyes curl openssh-server postfix &&
 systemctl start sshd postfix &&
 systemctl enable sshd postfix &&
 wget --output-document gitlab-ce-8.3.2-ce.0.el7.x86_64.rpm https://packages.gitlab.com/gitlab/gitlab-ce/packages/el/7/gitlab-ce-8.3.2-ce.0.el7.x86_64.rpm/download &&
-dnf install --assumeyes gitlab-ce-8.1.2-ce.0.el7.x86_64.rpm &&
+dnf install --assumeyes gitlab-ce-8.3.2-ce.0.el7.x86_64.rpm &&
 gitlab-ctl reconfigure &&
 firewall-cmd --permanent --add-service=http &&
 firewall-cmd --reload &&
