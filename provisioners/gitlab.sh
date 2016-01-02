428bcd7564178a262ed875421c7df9f66048cb18
Emory Merryman <emory.merryman@gmail.com>
2016-01-02 05:23:44 -0500

    using wget instead of curl

--- provisioners/gitlab.sh
+++ provisioners/gitlab.sh
@@ -8,4 +8,51 @@ dnf install --assumeyes gitlab-ce-8.3.2-ce.0.el7.x86_64.rpm &&
 gitlab-ctl reconfigure &&
 firewall-cmd --permanent --add-service=http &&
 firewall-cmd --reload &&
+
+ls -1rt /var/opt/gitlab/backups/ | tail --lines 1 | sed -e "s#_gitlab_backup.tar\$##" | while read TSTAMP
+do
+echo yes | gitlab-rake gitlab:backup:restore BACKUP=${TSTAMP} &&
+true
+done &&
+
+(cat > /usr/local/sbin/indigotire.sh <<EOF
+#!/usr/bin/bash
+
+/usr/bin/gitlab-rake gitlab:backup:create
+ls -1tr /var/opt/gitlab/backups | head --lines -10 | while read FILE
+do
+if [ $(60 * 60) -lt $(($(date +%s) - $(stat --format %X /var/opt/gitlab/backups/${FILE}))) ]
+then
+rm /var/opt/gitlab/backups/${FILE} &&
+true
+fi &&
+true
+done &&
+EOF
+) &&
+chmod 0500 /usr/local/sbin/indigotire.sh &&
+(cat > /usr/lib/systemd/system/indigotire.service <<EOF
+[Unit]
+Description=Indigo Tire Gitlab Backup Service
+
+[Service]
+ExecStart=/usr/local/sbin/indigotire.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+) &&
+(cat > /usr/lib/systemd/system/indigotire.timer <<EOF
+[Unit]
+Description=Indigo Tire Gitlab Backup Timer
+
+[Timer]
+OnBootSec=1min
+OnUnitActiveSec=1min
+Unit=indigotire.service
+
+[Install]
+WantedBy=multi-user.target
+EOF
+) &&
 true
