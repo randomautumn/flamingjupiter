f85ba6fed32df4d2bc2d869f981f4a3edc4caaa5
Emory Merryman <emory.merryman@gmail.com>
2016-01-01 21:57:00 -0500

    added EOF heredoc tag

--- provisioners/gitlab.sh
+++ provisioners/gitlab.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-dnf install --assumeyes curl openssh-server &&
-systemctl enable sshd &&
+
+dnf install --assumeyes openssh-server &&
+systemctl enable ssh &&d
 systemctl start sshd &&
 dnf install --assumeyes postfix &&
 systemctl enable postfix &&
 systemctl start postfix &&
+
+cd $(mktemp -d) &&
+curl -O https://downloads-packages.s3.amazonaws.com/centos-7.0.1406/gitlab-7.9.1_omnibus.1-1.el7.x86_64.rpm &&
+rpm -i gitlab-7.9.1_omnibus.1-1.el7.x86_64.rpm &&
+gitlab-ctl reconfigure &&
+cp /opt/gitlab/embedded/cookbooks/runit/files/default/gitlab-runsvdir.service /usr/lib/systemd/system &&
+
 firewall-cmd --permanent --add-service=http &&
 systemctl reload firewalld &&
 
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | bash &&
-dnf install --assumeyes gitlab-ce &&
 
-gitlab-ctl reconfigure &&
+
 true
