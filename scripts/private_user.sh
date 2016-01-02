ef9b3b67fc54b538fa9c278a44068c8dbfe2a4c8
Emory Merryman <emory.merryman@gmail.com>
2016-01-01 20:07:40 -0500

    added EOF heredoc tag

--- scripts/private_user.sh
+++ scripts/private_user.sh
@@ -12,19 +12,19 @@ cp /vagrant/private/pNZXklje_id_rsa /home/emory/.ssh &&
 chmod 0600 /home/emory/.ssh/pNZXklje_id_rsa &&
 gpg --allow-secret-key-import --import /vagrant/private/secret.gpg.key &&
 mkdir workspace &&
+cd /home/emory/workspace &&
 pass init BEAC2885 &&
 git config --global user.name "Emory Merryman" &&
 git config --global user.email "emory.merryman@gmail.com" &&
 git config --global user.signingkey BEAC2885 &&
 echo trusted-key AE695B97BEAC2885 >> /home/emory/.gnupg/gpg.conf &&
 pass git init &&
-cd workspace &&
-cd .. &&
-cd .password_store &&
-git remote add origin git@github.com:randomautumn/pinkparachute.git
-cd .. &&
+cd /home/emory/.password_store &&
+git remote add origin git@github.com:randomautumn/pinkparachute.git &&
+git fetch remote &&
+cd /home/emory &&
 git clone --branch v1.0.0 git@github.com:dirtyfrostbite/wildfish.git c9sdk &&
-cd c9sdk &&
+cd /home/emory/c9sdk &&
 export PATH=/opt/gcc/bin:${PATH} &&
 ./scripts/install-sdk.sh &&
 true
